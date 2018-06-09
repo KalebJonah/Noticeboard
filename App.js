482cbb7b56abbbc4083ff5cd41e8e83369ecd152
KalebJonah <jonathancaleb@rocketmail.com>
2018-06-09 04:56:12 -0700

    The noticeboard_React_App

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { StyleSheet, Platform, Image, Text, View } from 'react-native'
+import { SwitchNavigator } from 'react-navigation'
+import firebase from 'react-native-firebase'
+// import the different screens
+import Loading from './src/Loading'
+import SignUp from './src/Signup'
+import Login from './src/Login'
+import Main from './src/Main'
+// create our app's navigation stack
+const App = SwitchNavigator(
+  {
+    Loading,
+    SignUp,
+    Login,
+    Main
+  },
+  {
+    initialRouteName: 'Loading'
+  }
+)
+export default App 
+
